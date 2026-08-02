0244e371d89a6b50031a6b32f0a387b67112b0e7
Hammad <hammadbabar0326@gmail.com>
2026-08-02 21:42:03 +0500

    Add Pivot Index and RunningSum solution using Prefix Sum

---
+++ LeetCode Problems/PrefixSum/PivotIndex.java
@@ -0,0 +1,23 @@
+class PivotIndex {
+    public int pivot(int[] nums) {
+        int totalSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            totalSum += nums[i];
+        }
+        int leftSum = 0;
+        for (int j = 0; j < nums.length; j++) {
+            int rightSum = totalSum - leftSum - nums[j];
+            if (leftSum == rightSum) {
+                return j;
+            }
+            leftSum += nums[j];
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        PivotIndex p = new PivotIndex();
+        int arr[] = { 1, 7, 3, 6, 5, 6 };
+        System.out.println(p.pivot(arr));
+    }
+}
