858363aa1e0b8188396731d0fbc53fd3040d097e
Hammad <hammadbabar0326@gmail.com>
2026-08-10 22:20:20 +0500

    Change 3Sum cpp file to 3Sum.java

--- LeetCode Problems/Array/TwoPointers_Sorting/15_3Sum.cpp
+++ LeetCode Problems/Array/TwoPointers_Sorting/15_3Sum.cpp
@@ -1,33 +1,36 @@
 class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
-
-        for(int i=0; i<n; i++){
-            if(i>0 && nums[i] == nums[i-1]) continue;
-                int j = i+1;
-                int k= n-1;
-
-                while(j<k){
-                    int sum = nums[i]+nums[j]+nums[k];
-                    if(sum<0){
-                     j++;
-                    }else if(sum>0){
-                     k--;
-                    }else{
-                ans.push_back({nums[i], nums[j], nums[k]});
-                j++;
-                k--;
-        
-                while(j<k && nums[j] == nums[j-1]) j++;
-                 while(j<k && nums[k] == nums[k+1]) k--;
-                    
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int first = i;
+            int second = i + 1;
+            int third = nums.length - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            while (second < third) {
+                List<Integer> triplets = new ArrayList<>();
+                if (nums[first] + nums[second] + nums[third] == 0) {
+                    triplets.add(nums[first]);
+                    triplets.add(nums[second]);
+                    triplets.add(nums[third]);
+                    list.add(triplets);
+                    second++;
+                    third--;
+                    while (second < third && nums[second] == nums[second - 1]) {
+                        second++;
+                    }
+                    while (second < third && nums[third] == nums[third + 1]) {
+                        third--;
+                    }
+                } else if (nums[first] + nums[second] + nums[third] < 0) {
+                    second++;
+                } else {
+                    third--;
                 }
             }
         }
-        return ans;
+        return list;
     }
-    
-};
+}
