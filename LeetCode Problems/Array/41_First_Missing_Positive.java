740f50c10258b39e7e5be960f1f6dd973cf01466
Hammad <hammadbabar0326@gmail.com>
2026-08-17 16:47:38 +0500

    Resubmit 41. First Missing Positive

--- LeetCode Problems/Array/41_First_Missing_Positive.java
+++ LeetCode Problems/Array/41_First_Missing_Positive.java
@@ -1,9 +1,12 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        Arrays.sort(nums);
+        HashSet<Integer> set = new HashSet<>();
         int answer = 1;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == answer) {
+            if (nums[i] > 0) {
+                set.add(nums[i]);
+            }
+            while (set.contains(answer)) {
                 answer++;
             }
         }
