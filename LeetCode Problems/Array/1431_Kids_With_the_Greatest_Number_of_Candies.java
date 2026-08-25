cc43aa0e524ed234f998abcae99c0456a2bb7c9e
Hammad Babar <hammadbabar0326@gmail.com>
2026-08-25 20:21:34 +0500

    Resubmit 1431. Kids With the Greatest Number of Candies

--- LeetCode Problems/Array/1431_Kids_With_the_Greatest_Number_of_Candies.java
+++ LeetCode Problems/Array/1431_Kids_With_the_Greatest_Number_of_Candies.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         List<Boolean> result = new ArrayList<>();
-        int maxElement = 0;
-        for(int num:candies){
-            if(num > maxElement){
-                maxElement = num;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
             }
         }
-        for(int num:candies){
-            if(num + extraCandies >= maxElement){
-                 result.add(true);
-            }else{
-                 result.add(false);
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] + extraCandies >= max) {
+                result.add(true);
+            } else {
+                result.add(false);
             }
         }
         return result;
