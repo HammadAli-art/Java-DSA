e4a700bb7aaaa0b7093608444e393bf01dd64792
Hammad Babar <hammadbabar0326@gmail.com>
2026-08-28 21:36:41 +0500

    Resubmit 27. Remove Element

--- LeetCode Problems/Array/27_Remove_Element.java
+++ LeetCode Problems/Array/27_Remove_Element.java
@@ -1,11 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
+        int left = 0;
         int k = 0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i] != val){
-                nums[k] = nums[i];
-                k ++;
+        while (left < nums.length) {
+            if (nums[left] != val) {
+                nums[k] = nums[left];
+                k++;
             }
+            left++;
         }
         return k;
     }
