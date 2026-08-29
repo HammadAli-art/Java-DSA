33187d459269362478ebd81f39bae4ae7824dad2
Hammad Babar <hammadbabar0326@gmail.com>
2026-08-29 17:08:37 +0500

    Resubmit 643. Maximum Average Subarray I

--- LeetCode Problems/Array/643_Maximum_Average_Subarray_I.java
+++ LeetCode Problems/Array/643_Maximum_Average_Subarray_I.java
@@ -1,18 +1,19 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-        int currentSum = 0;
-        int maxSum = 0;
-        double average = 0;
-        for (int i = 0; i < k; i++) {
-            currentSum += nums[i];
-        } 
-         maxSum = currentSum;
-        for (int i = k; i < nums.length; i++) {
-            currentSum = currentSum - nums[i - k] + nums[i];
-            if (currentSum > maxSum) {
-                maxSum = currentSum;
+        int sum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int left = 0;
+        for (int right = 0; right < nums.length; right++) {
+            if (right < k) {
+                sum += nums[right];
+            } else {
+                sum = sum - nums[left] + nums[right];
+                left++;
+            }
+            if (right >= k - 1) {
+                maxSum = Math.max(maxSum, sum);
             }
         }
-        return (double)maxSum/k;
+        return (double) maxSum / k;
     }
 }
