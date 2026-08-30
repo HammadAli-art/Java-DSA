85b6dbec03bd93c0c576f2362d5ed66b057d5f75
Hammad Babar <hammadbabar0326@gmail.com>
2026-08-30 17:32:41 +0500

    Resubmit 974. Subarray Sums Divisible by K

--- LeetCode Problems/Array/974_Subarray_Sums_Divisible_by_K.java
+++ LeetCode Problems/Array/974_Subarray_Sums_Divisible_by_K.java
@@ -1,22 +1,18 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int prefixSum[] = new int[nums.length];
         HashMap<Integer, Integer> map = new HashMap<>();
         int count = 0;
-        prefixSum[0] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            prefixSum[i] = prefixSum[i - 1] + nums[i];
-        }
+        int sum = 0;
         map.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
-            int reminder = prefixSum[i] % k;
+            sum += nums[i];
+            int reminder = sum % k;
             if (reminder < 0) {
                 reminder += k;
             }
             if (map.containsKey(reminder)) {
-                int frequency = map.get(reminder);
-                map.put(reminder, frequency + 1);
-                count += frequency;
+                count += map.get(reminder);
+                map.put(reminder, map.get(reminder) + 1);
             } else {
                 map.put(reminder, 1);
             }
