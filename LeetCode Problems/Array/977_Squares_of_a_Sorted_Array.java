12d1dc368bacf986e4978ca247d1dc24b1676c29
Hammad Babar <hammadbabar0326@gmail.com>
2026-08-31 19:43:38 +0500

    Resubmit 977. Squares of a Sorted Array

--- LeetCode Problems/Array/977_Squares_of_a_Sorted_Array.java
+++ LeetCode Problems/Array/977_Squares_of_a_Sorted_Array.java
@@ -5,16 +5,15 @@ class Solution {
         int index = nums.length - 1;
         int result[] = new int[nums.length];
 
-        while(left <= right){
-            if(nums[left]*nums[left] > nums[right]*nums[right]){
-                result[index] = nums[left]*nums[left];
-                left ++;
-                index --;
-            }else{
-                result[index] = nums[right]*nums[right];
-                right --;
-                index --;
+        while (left <= right) {
+            if (nums[left] * nums[left] > nums[right] * nums[right]) {
+                result[index] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[index] = nums[right] * nums[right];
+                right--;
             }
+            index--;
         }
         return result;
     }
