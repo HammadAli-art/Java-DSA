7dde93f3173e381528e919239d52da2df95d6a84
Hammad Babar <hammadbabar0326@gmail.com>
2026-09-01 18:22:55 +0500

    Resubmit 744. Find Smallest Letter Greater Than Target

--- LeetCode Problems/Array/744_Find_Smallest_Letter_Greater_Than_Target.java
+++ LeetCode Problems/Array/744_Find_Smallest_Letter_Greater_Than_Target.java
@@ -2,18 +2,17 @@ class Solution {
     public char nextGreatestLetter(char[] letters, char target) {
         int left = 0;
         int right = letters.length - 1;
-
-        while(left <= right){
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(letters[mid] <= target){
+            if (letters[mid] <= target) {
                 left = mid + 1;
-            }else{
-                right = mid - 1;
+            } else {
+                right = mid;
             }
         }
-        if(left == letters.length){
-        return letters[0];
+        if (letters[left] > target) {
+            return letters[left];
         }
-        return letters[left];
+        return letters[0];
     }
 }
