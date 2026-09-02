864f4f669feb9df185f08492d40c78a82447830c
Hammad Babar <hammadbabar0326@gmail.com>
2026-09-02 16:49:45 +0500

    Resubmit 219. Contains Duplicate II

--- LeetCode Problems/Array/219_Contains_Duplicate_II.java
+++ LeetCode Problems/Array/219_Contains_Duplicate_II.java
@@ -1,14 +1,11 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for(int i=0; i<nums.length; i++){
-            if(m.containsKey(nums[i])){
-                int previousIndex = m.get(nums[i]);
-                if(i-previousIndex <= k){
-                    return true;
-                }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i]) && i - map.get(nums[i]) <= k) {
+                return true;
             }
-            m.put(nums[i], i);
+            map.put(nums[i], i);
         }
         return false;
     }
