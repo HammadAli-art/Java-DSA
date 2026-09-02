865c1210c11309d2199ddca30953d01c6ee32aa1
Hammad Babar <hammadbabar0326@gmail.com>
2026-09-02 18:25:22 +0500

    Resubmit 350. Intersection of Two Arrays II

--- LeetCode Problems/Array/350_Intersection_of_Two_Arrays_II.java
+++ LeetCode Problems/Array/350_Intersection_of_Two_Arrays_II.java
@@ -3,25 +3,21 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<nums1.length; i++){
-            if(map.containsKey(nums1[i])){
-            int frequency = map.get(nums1[i]);
-            map.put(nums1[i], frequency+1);
-            }else{
-            map.put(nums1[i], 1);
+        for (int i = 0; i < nums1.length; i++) {
+            if (map.containsKey(nums1[i])) {
+                map.put(nums1[i], map.get(nums1[i]) + 1);
+            } else {
+                map.put(nums1[i], 1);
             }
         }
-        for(int j=0; j<nums2.length; j++){
-            if(map.containsKey(nums2[j])){
-                int frequency = map.get(nums2[j]);
-                if(frequency > 0){
+        for (int j = 0; j < nums2.length; j++) {
+            if (map.containsKey(nums2[j]) && map.get(nums2[j]) > 0) {
                 list.add(nums2[j]);
-                map.put(nums2[j], frequency-1);
-                }
+                map.put(nums2[j], map.get(nums2[j]) - 1);
             }
         }
         int result[] = new int[list.size()];
-        for(int k=0; k<list.size(); k++){
+        for (int k = 0; k < list.size(); k++) {
             result[k] = list.get(k);
         }
         return result;
