eade54db6083f967319d9833175a18624c114c9c
Hammad Babar <hammadbabar0326@gmail.com>
2026-09-02 17:18:30 +0500

    Resubmit 349. Intersection of Two Arrays

--- LeetCode Problems/Array/349_Intersection_of_Two_Arrays.java
+++ LeetCode Problems/Array/349_Intersection_of_Two_Arrays.java
@@ -1,19 +1,19 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
         ArrayList<Integer> list = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        HashSet<Integer> set = new HashSet<>();
 
-        for(int i=0; i<nums1.length; i++){
-            map.put(nums1[i], i);
+        for (int i = 0; i < nums1.length; i++) {
+            set.add(nums1[i]);
         }
-        for(int j=0; j<nums2.length; j++){
-            if(map.containsKey(nums2[j])){
+        for (int j = 0; j < nums2.length; j++) {
+            if (set.contains(nums2[j])) {
                 list.add(nums2[j]);
-                map.remove(nums2[j]);
+                set.remove(nums2[j]);
             }
         }
         int result[] = new int[list.size()];
-        for(int i=0; i<list.size(); i++){
+        for (int i = 0; i < list.size(); i++) {
             result[i] = list.get(i);
         }
         return result;
