4a115c9a23dcc5e0a2b0874850f7c3a3a2d1c219
Hammad Babar <hammadbabar0326@gmail.com>
2026-09-04 22:38:56 +0500

    Resubmit 268. Missing Number

--- LeetCode Problems/Array/268_Missing_Number.java
+++ LeetCode Problems/Array/268_Missing_Number.java
@@ -1,20 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int left = 0;
-        while (left < nums.length) {
-            if (nums[left] < nums.length && nums[left] != nums[nums[left]]) {
-                int temp = nums[left];
-                nums[left] = nums[temp];
-                nums[temp] = temp;
-            } else {
-                left++;
-            }
-        }
+        int n = nums.length;
+        int sum = n * (n + 1) / 2;
+        int arrSum = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != i) {
-                return i;
-            }
+            arrSum += nums[i];
         }
-        return nums.length;
+        sum -= arrSum;
+        return sum;
     }
 }
